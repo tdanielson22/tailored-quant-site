a03eb28cd74ad3ad54956765e9ee149ff466a1c5
Tom Danielson <tdanielson22@hotmail.com>
2026-09-14 15:31:06 -0400

    add gitignore, remove generated site folder

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome",
-        "excerpt":"This is a placeholder post. Delete or replace it once you’re writing for real. Posts live in _posts/ as files named YYYY-MM-DD-title.md — Jekyll picks them up automatically and lists them on the Writing page.  ","categories": [],
-        "tags": [],
-        "url": "/2026/07/24/welcome.html",
-        "teaser": null
-      }]
